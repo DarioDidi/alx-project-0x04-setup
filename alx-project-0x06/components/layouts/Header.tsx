1c5c74dc5528cf0774331c1bd36bae93126f2459
Dario Didi <dididario.dd2@gmail.com>
2025-06-15 14:13:47 +0300

    fix header

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -3,40 +3,47 @@
 import Link from "next/link";
 import Button from "../common/Button";
 import { usePathname } from "next/navigation";
-import { useSelector } from "react-redux";
 import { RootState } from "@/store/store";
+import { useSelector } from "react-redux";
 
 const Header: React.FC = () => {
-	const pathname = usePathname();
-	//const { count } = useCount()
+
+	const pathname = usePathname()
 	const count = useSelector((state: RootState) => state.counter.value)
+
+
 	return (
 		<header className="fixed w-full bg-white shadow-md">
 			<div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
-				<Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight">
+				<Link href="/" className="text-3xl font-bold text-gray-800 tracking-tight">
 					Splash App
 				</Link>
+				<Link href="/counter-app" className="text-3xl font-bold text-gray-800 tracking-tight">
+					Counter App
+				</Link>
 
-				{/*Button Group */}
+				{/* Button Group */}
 				<div className="flex gap-4">
 					{
-						!["/counter-app"].includes(pathname)
-							? (
-								<>
-									<Button buttonLabel="Sign In" buttonBackgroundColor="red" />
-									<Button
-										buttonLabel="Sign Up"
-										buttonBackgroundColor="blue"
-									/>
-								</>
-							) : (
-								<p className="font-semibold text-lg">Current count : {count}</p>
-							)
+						!["/counter-app"].includes(pathname) ? (
+							<>
+								<Button
+									buttonLabel="Sign In"
+									buttonBackgroundColor="red"
+								/>
+								<Button
+									buttonLabel="Sign Up"
+									buttonBackgroundColor="blue"
+								/></>
+						) : (
+							<p className=" font-semibold text-lg">Current count : {count}</p>
+						)
 					}
+
 				</div>
 			</div>
 		</header>
-	)
+	);
 };
 
 export default Header;
--- alx-project-0x06/pages/counter-app.tsx
+++ alx-project-0x06/pages/counter-app.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import { AppDispatch, useAppDispatch, RootState, increment, decrement } from "@/store/store";
 
